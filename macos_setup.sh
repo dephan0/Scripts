09e9290f59dbd02565dda2146e16728a878ce981
Szymon Hankus <dephan@protonmail.com>
2023-03-10 16:19:13 +0100

    Tweaked macOS setup

--- macos_setup.sh
+++ macos_setup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# echo "Disabling the chime on startup..."
-# sudo nvram StartupMute=%01 && echo Done.
+echo "Disabling the chime on startup..."
+sudo nvram StartupMute=%01 && echo "Done."
 
-# echo "Removing the 'recents' section from the dock..."
-# defaults write com.apple.dock 'show-recents' -bool 'false' && echo Done.
+echo "Removing the 'recents' section from the dock..."
+defaults write com.apple.dock 'show-recents' -bool 'false' && echo "Done."
 
 
 # DOCK SETUP
+echo "Setting up the dock..."
 dock_item_plist () {
 		local path="$1"
 	    printf '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>%s</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>', "$path" 
@@ -34,4 +35,6 @@ defaults write com.apple.dock 'mineffect' -string 'scale' # use the 'scale' anim
 defaults write com.apple.dock 'autohide' -bool 'true'
 defaults write com.apple.dock 'autohide-delay' -float '0.0'
 defaults write com.apple.dock 'autohide-time-modifier' -float '0.4'
+echo "Done."
+
 
