f5934d658d4cf8d100ead75ba2ad9f67262f6b45
Szymon Hankus <dephan@protonmail.com>
2023-04-04 22:26:11 +0200

    Improved background changing script
    The script now stores the previously used backgrounds in a separate
    file (already_used) to reduce repetition.
    
    When choosing the next random background, the script reads the file
    to see if the background is already there, and if it is - omits it.
    
    If the already_used file contains all the possible images (is full),
    a random one is chosen, excluding the last one from already_used
    (in order to avoid repetition).

--- gnome_set_random_bg.sh
+++ gnome_set_random_bg.sh
@@ -1,34 +1,35 @@
 #!/bin/sh
 
-# PICTURE_DIR can be chosen
+# PICTURE_DIR can be chosen (argument $1)
 PICTURE_DIR="${1:-$HOME/.local/share/backgrounds}"
+ALREADY_USED="$PICTURE_DIR/already_used"
+[ ! -f "$ALREADY_USED" ] && touch "$ALREADY_USED"
 
-# A random picture from the PICTURE_DIR
-get_random () {
-	local random="$(find "$PICTURE_DIR" -type f \( -name '*.png' -o -name '*.jpg' \) -exec realpath {} \; | shuf -n1)"
+# A random picture from the PICTURE_DIR, which is not in the already_used file
+get_new_random () {
+	local pictures="$(find "$PICTURE_DIR" -type f \( -name '*.png' -o -name '*.jpg' \) -exec realpath {} \;)"
+	local not_used="$({ echo "$pictures"; cat "$ALREADY_USED"; } | sort | uniq -u)"  # Get only the files which appear
+																					 # in $pictures and not in alread_used.
+																					 # Won't work if already_used is tampered with.
 
-	echo $random
-}
-
-# A random picture from the PICTURE_DIR, which is not the same as the current wallpaper
-get_different_random () {
-	local current="$(gsettings get org.gnome.desktop.background picture-uri)"
-	# trim the leading 'file:// and the trailing ' to get just the path
-	current="${current#\'file://}"  
-	current="${current%\'}" 
-
-	local random="$(get_random)"
-	while [ "$random" = "$current" ]; do  # keep randomising until you get a new file
-		random="$(get_random)"
-	done
+	if [ -z "$not_used" ]; then  # Image base exhausted. Everything in already_used
+		local random="$(cat "$ALREADY_USED" | head --lines=-1 | shuf -n1)"  # get a random line from already_used (which now contains 
+																			# every picture) but exclude the last one (to avoid 
+																			# repeating backgrounds)
+		echo $random > $ALREADY_USED  # reset the already_used file
+	else
+		local random="$(echo "$not_used" | shuf -n1)"
+		echo $random >> $ALREADY_USED
+	fi
 
 	echo $random
 }
 
 
 main () {
-	local new="$(get_different_random)"
+	local new="$(get_new_random)"
 	gsettings set org.gnome.desktop.background picture-uri "file://$new"
+	# echo $new
 }
 
 main
